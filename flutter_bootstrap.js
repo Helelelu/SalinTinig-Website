a859071f706e6a4b67156f932f7bc2b8b16a3ce4
Helelelu <haellarenz@gmail.com>
2026-05-04 00:56:24 +0800

    feat: latest web version
    Final Commit:
    - Improved STT
    - Minor UI revisions

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "504744791" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2075190465" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
